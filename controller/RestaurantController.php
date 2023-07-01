f4b73c9bb95f40dddd9ead9ef34c10d36156ebeb
Frederic Branca <frederic.branca@gmail.com>
2023-07-01 18:32:44 +0200

    add method afficherCarte()

--- controller/RestaurantController.php
+++ controller/RestaurantController.php
@@ -7,7 +7,7 @@ use Model\Connect; // "use" pour accéder à la classe Connect située dans le n
 
 class RestaurantController
 {
-    /* Afficher la carte et menus */
+    /* Afficher les horaires' */
     public function afficherHoraire()
     {
         $pdo = Connect::seConnecter();
@@ -19,4 +19,44 @@ class RestaurantController
 
         require "view/accueil.php";
     }
+
+    /* Afficher la carte et menus' */
+    public function afficherCarte()
+    {
+        $pdo = Connect::seConnecter();
+
+        $requeteDejeuner = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'dejeuner'
+        ");
+        $requeteSoir = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'soir'
+        ");
+        $requeteEntree = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'entree'
+        ");
+        $requetePlat = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'plat'
+        ");
+        $requeteDessert = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'dessert'
+        ");
+        $requeteBoisson = $pdo -> query("
+            SELECT *, FORMAT(prix, 2) AS prix_format
+            FROM carte
+            WHERE section = 'boisson'
+        ");
+
+        require "view/carte.php";
+    }
+
 }
