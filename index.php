a4a9146ac918bc62b2b6e0e877d2e605753c5921
Frederic Branca <frederic.branca@gmail.com>
2023-07-01 15:44:37 +0200

    display accueil.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start();
+
+use Controller\RestaurantController; // On "use" le controller Restaurant
+
+// On autocharge les classes du projet
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+$ctrlRestaurant = new RestaurantController(); // On instancie le controller Restaurant
+
+$id = (isset($_GET["id"])) ? $_GET["id"] : null;
+
+if(isset($_GET["action"])) {
+
+    switch ($_GET["action"]) {
+
+        // RestaurantController
+        case "accueil" : $ctrlRestaurant -> afficherHoraire(); break;
+
+    }
+}
