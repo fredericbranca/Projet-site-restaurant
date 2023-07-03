893247079ee1bf33f54ef2a97d2c20c30036484a
Frederic Branca <frederic.branca@gmail.com>
2023-07-04 00:49:26 +0200

    add query for add product to menu

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -69,7 +69,7 @@ class AdminController
 
             $prix = filter_input(INPUT_POST, 'prix', FILTER_VALIDATE_FLOAT);
             // On vérifie si la longueur du produit est correct
-            if ($prix <=0 || empty($prix)) {
+            if ($prix <= 0 || empty($prix)) {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Le prix minimale est de 0,01€</div>";
                 header("Location: index.php?action=admin");
                 exit;
@@ -109,6 +109,60 @@ class AdminController
             } else {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Champs incorrect</div>";
                 header("Location: index.php?action=admin");
+                exit;
+            }
+        }
+
+        // Ajouter un produit à la carte
+
+        if (isset($_POST['ajouter']) && isset($_GET['section'])) {
+
+            // Filtre
+
+            $section = filter_input(INPUT_GET, 'section', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            // Vérification de la section
+            $sectionsAutorisees = ["dejeuner", "soir", "entree", "plat", "dessert", "boisson"];
+            if (!in_array($section, $sectionsAutorisees)) {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur URL section</div>";
+                header("Location: index.php?action=admin");
+                exit;
+            }
+
+            $produit = filter_input(INPUT_POST, 'produit', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            // On vérifie si la longueur du produit est correct
+            if (strlen($produit) <= 0 || strlen($produit) > 255 || empty($produit)) {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Le produit doit contenir entre 1 et 255 caractères.</div>";
+                header("Location: index.php?action=admin#$section");
+                exit;
+            }
+
+            $prix = filter_input(INPUT_POST, 'prix', FILTER_VALIDATE_FLOAT);
+            // On vérifie si la longueur du produit est correct
+            if ($prix <= 0 || empty($prix)) {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Le prix minimale est de 0,01€</div>";
+                header("Location: index.php?action=admin#$section");
+                exit;
+            }
+
+            if ($produit !== false && $prix !== false && $section !== false) {
+
+                // Requete pour ajouter le produit entrée
+                $requeteAjoutProduit = $pdo->prepare("
+                    INSERT INTO carte (description, prix, section)
+                    VALUES (:description, :prix, :section)
+                ");
+                $requeteAjoutProduit->execute([
+                    'description' => $produit,
+                    'prix' => $prix,
+                    'section' => $section
+                ]);
+
+                $_SESSION['alerte'] = "<div id='alert' class='alert-green'>Le produit a été ajouté</div>";
+                header("Location: index.php?action=admin#$section");
+                exit;
+            } else {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Champs incorrect</div>";
+                header("Location: index.php?action=admin#$section");
                 exit;
             }
         }
