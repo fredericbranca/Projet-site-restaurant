8ff503945c81a2a4e9200af4aa278466a3b963f1
Frederic Branca <frederic.branca@gmail.com>
2023-07-05 19:08:23 +0200

    panier view (display product)

--- view/panier.php
+++ view/panier.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['message'])) {
 
 $panier = $requetePanier->fetch();
 
-if (empty($panier)) {
+if (!$panier) {
 ?>
     <div class="column header-link">
         <div class="titre">PANIER</div>
@@ -22,26 +22,82 @@ if (empty($panier)) {
         </div>
     </div>
 
-<div class="panier-vide">
-    <div>Votre panier est vide. <a href="index.php?action=livraison">Retour à la commande</a></div>
-</div>
+    <div class="panier-vide">
+        <div>Votre panier est vide. <a href="index.php?action=livraison">Retour à la commande</a></div>
+    </div>
 
 <?php
 } else {
+    $produits = $requeteProduits->fetchAll();
 ?>
 
-<div class="column header-link">
-    <div class="titre">PANIER</div>
-    <div class="link wrap">
-        <a href="index.php?action=accueil">Accueil</a>
-        <div>></div>
-        <div>Votre panier</div>
+    <div class="column header-link">
+        <div class="titre">PANIER</div>
+        <div class="link wrap">
+            <a href="index.php?action=livraison">Livraison : Carte</a>
+            <div>></div>
+            <div>Votre panier</div>
+        </div>
     </div>
-</div>
 
-<div class="panier-vide">
-    <div>PANIER</div>
-</div>
+    <div class="content">
+        <div class="tableau">
+            <div class="item">
+                <div>Plat</div>
+                <div class="prix">Prix</div>
+                <div class="quantite">Quantité</div>
+                <div class="prixTotal">Total</div>
+            </div>
+            <div class="ligne"></div>
+
+            <?php
+            $total = 0;
+            foreach ($produits as $produit) {
+                $total += $produit['quantite'] * $produit['prix'];
+            ?>
+
+                <div class="item">
+                    <div><?= $produit['description'] ?></div>
+                    <div class="prix"><?= $produit['prix'] ?> €</div>
+                    <div>
+                        <form class="qtt" method="POST" action="index.php?action=panier&id=<?=$produit['id_produit'] ?>" enctype="multipart/form-data">
+                            <button class="btn" type="submit" name="moins" id="submit">-</button>
+                            <?= $produit['quantite'] ?>
+                            <button class="btn" type="submit" name="plus" id="submit">+</button>
+                        </form>
+                    </div>
+                    <div class="prixTotal"><?= $produit['quantite'] * $produit['prix'] ?>€</div>
+                </div>
+                <div class="ligne"></div>
+
+            <?php
+            }
+            ?>
+        </div>
+        <div class="fin">
+            <div class="adresse">
+                <div>Adresse de livraison</div>
+                <select name="test" id="test">
+                    <option value="">Please choose an option</option>
+                    <option value="1">Bonjour</option>
+                    <option value="2">Test</option>
+                </select>
+                <a class="bouton btn-register" href="index.php?action=adresse"><span>NOUVELLE ADRESSE</span></a>
+            </div>
+            <div class="payer">
+                <div class="livraison">
+                    <div>Livraison</div>
+                    <div>3 €</div>
+                </div>
+                <div class="total">
+                    <div>Total</div>
+                    <div><?= $total + 3 ?> €</div>
+                </div>
+                <input class="bouton input-btn" type="submit" name="submit" value="PAYER">
+                <img src="public/img/sous-bouton.png">
+            </div>
+        </div>
+    </div>
 
 <?php
 }
