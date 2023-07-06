0f33b44f5526a950ed3ec0a24db7c193c1494d3d
Frederic Branca <frederic.branca@gmail.com>
2023-07-06 05:23:41 +0200

    change an address

--- controller/ProfilController.php
+++ controller/ProfilController.php
@@ -30,7 +30,7 @@ class ProfilController
         ]);
 
         // Ajouter une adresse
-        if(isset($_POST['ajouter'])) {
+        if (isset($_POST['ajouter'])) {
 
             // Filtre
             $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -40,7 +40,7 @@ class ProfilController
             $ville = filter_input(INPUT_POST, 'ville', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $numero = filter_input(INPUT_POST, 'num', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[0-9]{10}$/'))); //vérifie que le numéro de téléphone est composé exactement de 10 chiffres de 0 à 9
             $defaut = filter_input(INPUT_POST, 'defaut', FILTER_VALIDATE_BOOLEAN);
-            if($defaut) {
+            if ($defaut) {
                 $defaut = 1;
             } else {
                 $defaut = 0;
@@ -53,7 +53,7 @@ class ProfilController
                     $adresseUser = $requeteAdresses->fetchAll();
                     $nbAdresse = count($adresseUser);
 
-                    if($defaut == 1) {
+                    if ($defaut == 1) {
                         $requeteChangeDefaut = $pdo->prepare("
                             UPDATE adresse
                             SET defaut = 0
@@ -82,27 +82,24 @@ class ProfilController
                     $_SESSION['alerte'] = "<div id='alert' class='alert-green'>Adresse ajouté avec succès</div>";
                     header("Location: index.php?action=profil&page=adresses");
                     exit;
-
                 } else {
                     $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Tous les champs sont obligatoires</div>";
                     header("Location: index.php?action=profil&page=ajouterAdresse");
                     exit;
                 }
-
             } else {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur champs</div>";
                 header("Location: index.php?action=profil&page=ajouterAdresse");
                 exit;
             }
-
         }
 
         // Supprimer une adresse
-        if(isset($_GET['page']) && $_GET['page'] === 'adresses' && isset($_GET['supprimer'])) {
+        if (isset($_GET['page']) && $_GET['page'] === 'adresses' && isset($_GET['supprimer'])) {
 
             $idAdresse = filter_input(INPUT_GET, 'supprimer', FILTER_VALIDATE_INT);
-            
-            if($idAdresse !== false) {
+
+            if ($idAdresse !== false) {
                 $requeteSupprimerAdresse = $pdo->prepare("
                     DELETE FROM adresse
                     WHERE id_user = :id_user AND id_adresse = :id_adresse
@@ -111,11 +108,10 @@ class ProfilController
                     'id_user' => $idUser,
                     'id_adresse' => $idAdresse
                 ]);
-            
+
                 $_SESSION['alerte'] = "<div id='alert' class='alert-green'>Adresse supprimé avec succès</div>";
                 header("Location: index.php?action=profil&page=adresses");
                 exit;
-                
             } else {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Une erreur s'est produit, l'adresse n'a pas été supprimée</div>";
                 header("Location: index.php?action=profil&page=adresses");
@@ -123,9 +119,91 @@ class ProfilController
             }
         }
 
-        
+        // Modifier une adresse
+
+        if (isset($_GET['page']) && $_GET['page'] === 'adresses' && isset($_GET['modifier'])) {
+
+            $idAdresse = $_GET['modifier'];
+
+            $requeteAdresseWithId = $pdo->prepare("
+                SELECT *
+                FROM adresse
+                WHERE id_user = :id_user AND id_adresse = :id_adresse
+            ");
+            $requeteAdresseWithId->execute([
+                'id_user' => $idUser,
+                'id_adresse' => $idAdresse
+            ]);
+        }
+
+        if (isset($_POST['modifier']) && isset($_GET['id'])) {
+
+            
+            // Filtre
+            $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $adresse = filter_input(INPUT_POST, 'adresse', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $CP = filter_input(INPUT_POST, 'CP', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $ville = filter_input(INPUT_POST, 'ville', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $numero = filter_input(INPUT_POST, 'num', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[0-9]{9,10}$/'))); //vérifie que le numéro de téléphone est composé exactement de 10 chiffres de 0 à 9
+            $defaut = filter_input(INPUT_POST, 'defaut', FILTER_VALIDATE_BOOLEAN);
+            if ($defaut) {
+                $defaut = 1;
+            } else {
+                $defaut = 0;
+            }
+            $idAdresse = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+            if ($nom !== false && $prenom !== false && $adresse !== false && $CP !== false && $ville !== false && $numero !== false && $defaut !== false && $idAdresse !== false) {
+
+                if (!empty($nom) && !empty($prenom) && !empty($adresse) && !empty($CP) && !empty($ville) && !empty($numero) && !empty($idAdresse)) {
+
+                    $adresseUser = $requeteAdresses->fetchAll();
+                    $nbAdresse = count($adresseUser);
+
+                    if ($defaut == 1) {
+                        $requeteChangeDefaut = $pdo->prepare("
+                            UPDATE adresse
+                            SET defaut = 0
+                            WHERE id_user = :id_user
+                        ");
+                        $requeteChangeDefaut->execute([
+                            'id_user' => $idUser
+                        ]);
+                    }
+
+                    $requeteModifierAdresse = $pdo->prepare("
+                        UPDATE adresse
+                        SET id_user = :id_user, nom = :nom, prenom = :prenom, adresse = :adresse, cp = :cp, ville = :ville, telephone = :telephone, defaut = :defaut
+                        WHERE id_adresse = :id_adresse
+                    ");
+                    $requeteModifierAdresse->execute([
+                        'id_user' => $idUser,
+                        'nom' => $nom,
+                        'prenom' => $prenom,
+                        'adresse' => $adresse,
+                        'cp' => $CP,
+                        'ville' => $ville,
+                        'telephone' => $numero,
+                        'defaut' => $defaut,
+                        'id_adresse' => $idAdresse
+                    ]);
+
+                    $_SESSION['alerte'] = "<div id='alert' class='alert-green'>Adresse modifié avec succès</div>";
+                    header("Location: index.php?action=profil&page=adresses");
+                    exit;
+                } else {
+                    $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Tous les champs sont obligatoires</div>";
+                    header("Location: index.php?action=profil&page=adresses&modifier=$idAdresse");
+                    exit;
+                }
+            } else {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur champs</div>";
+                header("Location: index.php?action=profil&page=adresses&modifier=$idAdresse");
+                exit;
+            }
+        }
 
         require "view/profil.php";
     }
-
 }
