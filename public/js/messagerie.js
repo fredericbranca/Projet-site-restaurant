669fb3a881590de9c81bc230b760a5047759008b
Frederic Branca <frederic.branca@gmail.com>
2023-07-07 04:20:09 +0200

    script scrolling to a specific position

--- public/js/messagerie.js
+++ public/js/messagerie.js
@@ -1,5 +1,15 @@
+// Scroll à une position choisie
+function scrollToPosition(position) {
+    window.scrollTo({
+        top: position,
+        behavior: 'smooth' // 'smooth' pour un défilement fluide, 'auto' pour un défilement instantané
+    });
+}
+
+scrollToPosition(500);
+
 // Après le chargement de la page, positionne la barre de défilement en bas des messages
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     var messageList = document.getElementById('messageList');
     messageList.scrollTop = messageList.scrollHeight;
 });
