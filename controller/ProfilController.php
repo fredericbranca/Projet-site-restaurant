6d1774fbe0cfed52bf5da987361b794b83579d5b
Frederic Branca <frederic.branca@gmail.com>
2023-07-07 04:12:45 +0200

    query for mailbox (profile view)

--- controller/ProfilController.php
+++ controller/ProfilController.php
@@ -17,6 +17,9 @@ class ProfilController
 
         $pdo = Connect::seConnecter();
 
+        // Mettre en français
+        $requeteLangue = $pdo->query("SET lc_time_names = 'fr_FR';");
+
         $idUser = $_SESSION['users']['id'];
 
         // Afficher les adresses
@@ -154,7 +157,7 @@ class ProfilController
             } else {
                 $defaut = 0;
             }
-            
+
 
             if ($nom !== false && $prenom !== false && $adresse !== false && $CP !== false && $ville !== false && $numero !== false && $defaut !== false && $idAdresse !== false) {
 
@@ -218,12 +221,12 @@ class ProfilController
 
         // Afficher les réservations (pour l'admin) en fonction de la date
 
-        if(isset($_POST['dateRes'])) {
+        if (isset($_POST['dateRes'])) {
 
             $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-            if($date !== false) {
-                
+            if ($date !== false) {
+
                 $timestamp = strtotime($date);
                 $_SESSION['dateFormat'] = date('d - m - Y', $timestamp);
 
@@ -237,10 +240,9 @@ class ProfilController
                 ]);
 
                 $_SESSION['dateReservation'] = $requeteReservation->fetchAll();
-                
+
                 header("Location: index.php?action=profil&page=compte");
                 exit;
-
             } else {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur champs</div>";
                 header("Location: index.php?action=profil&page=compte");
@@ -250,11 +252,11 @@ class ProfilController
 
         // Annuler une réservation
 
-        if(isset($_POST['annuler']) && isset($_GET['id']) && isset($_SESSION['users']) && $_SESSION['users']['admin'] == 1) {
+        if (isset($_POST['annuler']) && isset($_GET['id']) && isset($_SESSION['users']) && $_SESSION['users']['admin'] == 1) {
 
             $idRes = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-            if($idRes !== false) {
+            if ($idRes !== false) {
 
                 $requeteSupprimerTable = $pdo->prepare("
                     DELETE FROM table_reserve
@@ -275,10 +277,129 @@ class ProfilController
                 $_SESSION['alerte'] = "<div id='alert' class='alert-green'>Réservation annulée avec succès</div>";
                 header("Location: index.php?action=profil&page=compte");
                 exit;
-
             } else {
                 $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Action non autorisée</div>";
                 header("Location: index.php?action=profil&page=compte");
+                exit;
+            }
+        }
+
+
+        // Envoyer message depuis session client
+        if ($_SESSION['users']['admin'] == 0 && isset($_GET['page']) && $_GET['page'] === 'messagerie' && !isset($_GET['id'])) {
+
+            // Afficher les messages
+            $requeteMessage = $pdo->prepare("
+                SELECT *, DATE_FORMAT(created_at, '%e %M %Y %H:%i') as created
+                FROM messagerie
+                WHERE conversation_id = :conversation_id
+                ORDER BY created_at ASC
+            ");
+            $requeteMessage->execute([
+                'conversation_id' => $idUser
+            ]);
+        }
+
+        if (isset($_POST['envoyerMessage']) && isset($_SESSION['users']) && $_SESSION['users']['admin'] == 0) {
+
+            //Filtre
+            $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if(empty($message)) {
+                $_SESSION['message'] = "<div class='alert-message'>Le contenu du message est vide !</div>";
+                header("Location: index.php?action=profil&page=messagerie");
+                exit;
+            }
+
+            if ($message !== false) {
+                $requeteSendMessage = $pdo->prepare("
+                    INSERT INTO messagerie (sender_id, receiver_id, message, conversation_id)
+                    VALUES (:sender_id, 12, :message, :conversation_id)
+                ");
+                $requeteSendMessage->execute([
+                    'sender_id' => $idUser,
+                    'message' => $message,
+                    'conversation_id' => $idUser
+                ]);
+                header("Location: index.php?action=profil&page=messagerie");
+                exit;
+            } else {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur message</div>";
+                header("Location: index.php?action=profil&page=messagerie");
+                exit;
+            }
+        }
+
+        // Afficher message depuis session admin
+        if ($_SESSION['users']['admin'] == 1 && isset($_GET['page']) && $_GET['page'] === 'messagerie') {
+
+            // Afficher les aperçus des messages
+            $requeteMessage = $pdo->query("
+                SELECT t1.*, DATE_FORMAT(t1.created_at, '%e %M %Y %H:%i') AS created, CONCAT(u.nom, ' ', u.prenom) as user
+                FROM messagerie t1
+                JOIN users u ON u.id = t1.conversation_id
+                LEFT JOIN messagerie t2 ON t1.conversation_id = t2.conversation_id AND t1.created_at < t2.created_at
+                WHERE t2.conversation_id IS NULL
+                ORDER BY t1.created_at DESC
+            ");
+        }
+
+        // affiche les messages par conversation depuis session admin
+        if ($_SESSION['users']['admin'] == 1 && isset($_GET['page']) && $_GET['page'] === 'messagerie' && isset($_GET['id'])) {
+
+            // Filtre
+
+            $idConv = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if ($idConv !== false && !empty($idConv)) {
+                // Afficher les messages
+                $requeteMessage = $pdo->prepare("
+                    SELECT *, DATE_FORMAT(created_at, '%e %M %Y %H:%i') as created
+                    FROM messagerie
+                    WHERE conversation_id = :conversation_id
+                    ORDER BY created_at ASC
+                ");
+                $requeteMessage->execute([
+                    'conversation_id' => $idConv
+                ]);
+            } else {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur message</div>";
+                header("Location: index.php?action=profil&page=messagerie&id=$idConv");
+                exit;
+            }
+        }
+
+        // Envoyer un message depuis session admin
+
+        if (isset($_POST['envoyerMessage']) && isset($_GET['id']) && isset($_SESSION['users']) && $_SESSION['users']['admin'] == 1) {
+
+            //Filtre
+            $idConv = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if(empty($message)) {
+                $_SESSION['message'] = "<div class='alert-message'>Le contenu du message est vide !</div>";
+                header("Location: index.php?action=profil&page=messagerie&id=$idConv");
+                exit;
+            }
+            
+
+            if ($message !== false && $idConv !== false && !empty($idConv)) {
+                $requeteSendMessage = $pdo->prepare("
+                    INSERT INTO messagerie (sender_id, receiver_id, message, conversation_id)
+                    VALUES (:sender_id, :receiver_id, :message, :conversation_id)
+                ");
+                $requeteSendMessage->execute([
+                    'sender_id' => $idUser,
+                    'receiver_id' => $idConv,
+                    'message' => $message,
+                    'conversation_id' => $idConv
+                ]);
+
+                header("Location: index.php?action=profil&page=messagerie&id=$idConv");
+                exit;
+
+            } else {
+                $_SESSION['alerte'] = "<div id='alert' class='alert-red'>Erreur message</div>";
+                header("Location: index.php?action=profil&page=messagerie&id=$idConv");
                 exit;
             }
         }
